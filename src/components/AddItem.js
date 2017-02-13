687f8ad0d487213bd9e5bf3109d2b32e798a0843
CRwming <btming217@gmail.com>
2017-02-13 13:55:52 +0800

    Update AddItem.js
    add event

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -6,17 +6,28 @@ import React, {Component,} from 'react';
 class AddItem extends Component {
    
     handleAdd = ()=> {
-       const text = this.refs.input;
+       const text = this.input;
         if (text.value.trim() === '')
             return;
         this.props.actions.addItem(text.value.trim());
         text.value = ''
     };
+               
+    handleKeyAdd = (e)=>{
+       if(e.key === 'Enter') {
+          const text = e.input;
+          if(text.value.trim() === ''){
+            return;
+          }
+          this.props.actions.addItem(text.value.trim());
+          text.value = '';
+       }
+    };           
 
     render() {
         return (
             <div>
-                <input type="text" ref='input'/>
+                <input type="text" ref={input=>this.input=input} onKeyPress={this.handleKeyAdd}/>
                 <button onClick={this.handleAdd}>添加</button>
             </div>
         );
